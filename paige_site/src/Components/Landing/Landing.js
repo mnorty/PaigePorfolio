8205f32961ef20768cfb3aeb7204d38717bad9a3
mnorty <mnorty@gmail.com>
2019-09-30 10:23:24 -0600

    seperation for first landing segment

--- paige_site/src/Components/Landing/Landing.js
+++ paige_site/src/Components/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Landing.css"
 
 class Landing extends Component {
   constructor(){
@@ -8,7 +9,25 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>Landing</h1>
+        <div className="pageDivision" id="one"></div>
+        <div className="pageDivision" id="two">
+          <div className="landingTitles">From Photo to finish, the process is easy and incredible.</div>
+          <div className="videoBody">
+            <div className="landingVideo">video container</div>
+          </div>
+        </div>
+        <div className="pageDivision" id="three">
+          <div>step container</div>
+        </div>
+        <div className="pageDivision" id="four">
+          <div className="landingTitles">testimonial title</div>
+          <div>body container
+            <div>left</div>
+            <div>right</div>
+          </div>
+        </div>
+        <div className="pageDivision" id="five"></div>
+        <div className="pageDivision" id="six"></div>
       </div>
     )
   }
